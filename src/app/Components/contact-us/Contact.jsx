09f1ffe03eb99e1b57383bc7bb1e60f1adf1e741
mohammedkamal-0 <mohammed0kamal1@gmail.com>
2024-02-02 17:01:53 +0300

    10 commit

--- src/app/Components/contact-us/Contact.jsx
+++ src/app/Components/contact-us/Contact.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import { IoIosCloseCircleOutline } from "react-icons/io";
 import { BiMailSend } from "react-icons/bi";
@@ -7,38 +8,127 @@ import "@/app/Components/slideMenu/slideMenu.css"
 export default function Contact() {
 
 
+    const [fullname, setFullname] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [error, setError] = useState([]);
+    const [success, setSuccess] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-  const [isOpen, setIsOpen] = useState(false);
-
-  var styles
-  if (isOpen) {
-      styles = 'flex'
-  } else {
-      styles = 'none'
-  }
-
-  
-  return (
-    <div>
-
-
-<botton onClick={() => { setIsOpen(true) }} className='flex items-center cursor-pointer gap-2 '>  <h1 className='bg-green-700 rounded-[10px]  text-5xl  text-[#ffffff] p-2 mt-8 ml-2'><BiMailSend /></h1>  <h1 className='mt-8'>Contact Us</h1> </botton>
+    var styles
+    if (isOpen) {
+        styles = 'flex'
+    } else {
+        styles = 'none'
+    }
 
 
-                        <div className={`fixed left-0 right-0 bottom-0 w-full h-0  ${isOpen ? "block" : "hidden"}`}>
-                            <div className={`fixed left-0 right-0 bottom-0 w-full h-full  bg-white duration-300 ${isOpen ? "h-full" : "h-0"}`} >
-                                <button onClick={() => { setIsOpen(false) }} className="text-black">  <IoIosCloseCircleOutline className='text-green-700' /> </button>
-                                <h1 className='flex items-center cursor-pointer gap-2 text-4xl text-green-700 mb-8'> <BiMailSend className='ml-8 mr-4 text-5xl' /> Contact Us </h1>
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-                                <ul>
-                                    <li className='py-6 border-b-4 text-3xl'>first</li>
-                                    <li className='py-6 border-b-4 text-3xl'>first</li>
-                                    <li className='py-6 border-b-4 text-3xl'>first</li>
-                                    <li className='py-6 border-b-4 text-3xl'>first</li>
-                                </ul>
-                            </div> </div>
+        console.log("Full name: ", fullname);
+        console.log("Email: ", email);
+        console.log("Message: ", message);
+
+        const res = await fetch("api/contact", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify({
+                fullname,
+                email,
+                message,
+            }),
+        });
+
+        const { msg, success } = await res.json();
+        setError(msg);
+        setSuccess(success);
+
+        if (success) {
+            setFullname("");
+            setEmail("");
+            setMessage("");
+        }
+    };
+
+    return (
+        <div>
 
 
-    </div>
-  )
+            <botton onClick={() => { setIsOpen(true) }} className='flex items-center cursor-pointer gap-2 '>  <h1 className='bg-green-700 rounded-[10px]  text-5xl  text-[#ffffff] p-2 mt-8 ml-2'><BiMailSend /></h1>  <h1 className='mt-8'>Contact Us</h1> </botton>
+
+
+            <div className={`fixed left-0 right-0 bottom-0 w-full h-0  ${isOpen ? "block" : "hidden"}`}>
+                <div className={`fixed left-0 right-0 bottom-0 w-full h-full  bg-white duration-300 ${isOpen ? "h-full" : "h-0"}`} >
+                    <button onClick={() => { setIsOpen(false) }} className="text-black">  <IoIosCloseCircleOutline className='text-green-700' /> </button>
+                    <h1 className='flex items-center cursor-pointer gap-2 text-4xl text-green-700 mb-8'> <BiMailSend className='ml-8 mr-4 text-5xl' /> Contact Us </h1>
+
+
+
+
+
+
+
+                    <div className="p-4 max-w-3xl mx-auto">
+                        <form
+                            onSubmit={handleSubmit}
+                            className="py-4 mt-4 border-t flex flex-col gap-5"
+                        >
+                            <div>
+                                <label htmlFor="fullname">Full Name</label>
+                                <input
+                                    onChange={(e) => setFullname(e.target.value)}
+                                    value={fullname}
+                                    type="text"
+                                    id="fullname"
+                                    placeholder="  mohammed kamal"
+                                />
+                            </div>
+
+                            <div className='mt-10'>
+                                <label htmlFor="email">Email</label>
+                                <input
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    value={email}
+                                    type="text"
+                                    id="email"
+                                    placeholder="  moha@gmail.com"
+                                />
+                            </div>
+
+                            <div className='mt-10'>
+                                <label htmlFor="message">Your Message</label>
+                                <textarea
+                                    onChange={(e) => setMessage(e.target.value)}
+                                    value={message}
+                                    className="h-32 w-full"
+                                    id="message"
+                                    placeholder="Type your message here..."
+                                ></textarea>
+                            </div>
+
+                            <button className="bg-green-700 p-3 text-white font-bold" type="submit">
+                                Send
+                            </button>
+                        </form>
+
+                        <div className="bg-slate-100 flex flex-col">
+                            {error &&
+                                error.map((e) => (
+                                    <div
+                                        className={`${success ? "text-green-800" : "text-red-600"
+                                            } px-5 py-2`}
+                                    >
+                                        {e}
+                                    </div>
+                                ))}
+                        </div>
+                    </div>
+                </div> </div>
+
+
+        </div>
+    )
 }
--- src/app/Components/map/Map.jsx
+++ src/app/Components/map/Map.jsx
@@ -1,24 +1,23 @@
 "use client"
 import React from 'react'
-import L, { icon } from 'leaflet'
-import { MapContainer, Marker, Popup, TileLayer , useMapEvents } from 'react-leaflet'
-import MarkerIcon from '../../../../node_modules/leaflet/dist/images/marker-icon.png'
-// import MarkerIcon from '../../../../node_modules/leaflet/dist/images/marker-shadow.png'
+// import L, { icon } from 'leaflet'
+// import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from 'react-leaflet'
+// import MarkerIcon from '../../../../node_modules/leaflet/dist/images/marker-icon.png'
+// // import MarkerIcon from '../../../../node_modules/leaflet/dist/images/marker-shadow.png'
 import 'leaflet/dist/leaflet.css'
 import { useEffect, useState } from "react";
-import { IoIosCloseCircleOutline } from "react-icons/io";
-import { FaBuildingColumns } from "react-icons/fa6";
+
 
 export default function Map() {
 
   const [isOpen, setIsOpen] = useState(false);
   var styles
   if (isOpen) {
-      styles = 'flex'
+    styles = 'flex'
   } else {
-      styles = 'none'
+    styles = 'none'
   }
-  
+
 
 
 
@@ -33,7 +32,7 @@ export default function Map() {
         map.flyTo(e.latlng, map.getZoom())
       },
     })
-  
+
     return position === null ? null : (
       <Marker position={position}
       >
@@ -41,59 +40,57 @@ export default function Map() {
       </Marker>
     )
   }
-  
-
-
 
   return (
-    <div>
-          <MapContainer style={{
-            margin:'0',
-            height:'80vh',
-            width:'100%'
-          }} center={[33.27406820466889, 44.37757743190237]} zoom={16} scrollWheelZoom={false}>
-          <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-            {/* <Marker 
-            
-            icon={
-              new L.Icon({
-                //icon pines
-                iconUrl:MarkerIcon.src,
-                iconRetinaUrl:MarkerIcon.src,
-                iconSize:[25 , 41],
-                iconAnchor:[12.5 , 41],
-                popupAnchor:[0 , -41],
-                // shadowUrl:MarkerShadow.src,
-                shadowSize:[41 , 41],
-              })}
-              position={[33.27406820466889, 44.37757743190237]}>
+    // <div>
+    //   <MapContainer style={{
+    //     zIndex:'50',
+    //     height: '80vh',
+    //     width: '100%'
+    //   }} center={[33.27406820466889, 44.37757743190237]} zoom={16} scrollWheelZoom={false}>
+    //     <TileLayer
+    //       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    //     />
+    //     <Marker
 
-                <Popup>
-                 <h1 className='text'>hhhhhhhh</h1>
-                </Popup>
-              </Marker> */}
+    //       icon={
+    //         new L.Icon({
+    //           //icon pines
+    //           iconUrl: MarkerIcon.src,
+    //           iconRetinaUrl: MarkerIcon.src,
+    //           iconSize: [25, 41],
+    //           iconAnchor: [12.5, 41],
+    //           popupAnchor: [0, -41],
+    //           // shadowUrl:MarkerShadow.src,
+    //           shadowSize: [41, 41],
+    //         })}
+    //       position={[33.27406820466889, 44.37757743190237]}>
 
-
-              {cities.map((city , idx) => (
-                <Marker 
-                position={[city.lat , city.long]}
-                icon={MarkerIcon}
-                key={idx}>
-                      <Popup>
-                        <p>{city.city} , </p>
-                      </Popup>
-                </Marker>
-              ))}
+    //       <Popup>
+    //         <h1 className='text'>hhhhhhhh</h1>
+    //       </Popup>
+    //     </Marker>
 
 
-              <LocationMarker />
-          </MapContainer>
-    </div>
+    //     {cities.map((city, idx) => (
+    //       <Marker
+    //         position={[city.lat, city.long]}
+    //         icon={MarkerIcon}
+    //         key={idx}>
+    //         <Popup>
+    //           <p>{city.city} , </p>
+    //         </Popup>
+    //       </Marker>
+    //     ))}
 
-    
+
+    //     <LocationMarker />
+    //   </MapContainer>
+    // </div>
+    <p>hh</p>
+
+
   )
 }
 
